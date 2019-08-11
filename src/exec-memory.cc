d15256a4009bf2afdb2353087c0c21fae0a90752
Steven Lariau <obs145628@gmail.com>
2019-08-11 14:05:45 +0200

    failed invalid memory size protection

--- src/exec-memory.cc
+++ src/exec-memory.cc
@@ -11,8 +11,8 @@ ExecMemory::ExecMemory(std::size_t len)
   _ptr_align = reinterpret_cast<char*>(  (reinterpret_cast<std::size_t> (_ptr + pagesize - 1)
 					  & ~(pagesize - 1)) );
 
-  if (mprotect(_ptr_align, 1024, PROT_READ|PROT_EXEC|PROT_WRITE))
-    throw std::runtime_error {"failed to change meory right accesses"};
+  if (mprotect(_ptr_align, len, PROT_READ|PROT_EXEC|PROT_WRITE))
+    throw std::runtime_error {"failed to change memory right accesses"};
 }
 
 ExecMemory::~ExecMemory()
